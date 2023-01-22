818d270cd5bc9df309a20dc9eeb1ad437655c79c
unknown <lucas.liuliongwah@mail.mcgill.ca>
2023-01-22 10:55:18 -0500

    Dashboard refresh

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,26 +1,8 @@
-import React, { useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
-import '../App.css';
-import './Dashboard.css';
-import BudgetCalculator from '../components/BudgetCalculator.js';
+import React from 'react';
 
 function Dashboard() {
-
-
-
-
-
   return (
     <div className="App">
-
-        {/* Budget Calculator */}
-        < BudgetCalculator />
-
-
-
-      <html lang="en">
-
-      <head>
           <meta name="viewport" content="width=device-width, initial-scale=1.0" />
           <title>Budget App</title>
           {/* Fonts */}
@@ -29,35 +11,33 @@ function Dashboard() {
           <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500&display=swap" rel="stylesheet" />
           {/* Login css */}
           <link rel="stylesheet" href="style.css" />
-      </head>
 
-      <body>
-          <div class="wrapper">
-              <div class="container">
-                  <div class="sub-container">
+          <div className="wrapper">
+              <div className="container">
+                  <div className="sub-container">
                       {/* <!-- Budget --> */}
-                      <div class="total-amount-container">
+                      <div className="total-amount-container">
                           <h3>Budget</h3>
-                          <p class="hide error" id="budget-error">
+                          <p className="hide error" id="budget-error">
                               Value cannot be empty or negative
                           </p>
                           <input type="number" id="total-amount" placeholder="Enter Total Amount" />
-                          <button class="submit" id="total-amount-button">Set Budget</button>
+                          <button className="submit" id="total-amount-button">Set Budget</button>
                       </div>
 
                       {/* <!-- Expenditure --> */}
-                      <div class="user-amount-container">
+                      <div className="user-amount-container">
                           <h3>Expenses</h3>
-                          <p class="hide error" id="product-title-error">
+                          <p className="hide error" id="product-title-error">
                               Values cannot be empty
                           </p>
-                          <input type="text" class="product-title" id="product-title" placeholder="Enter Title of Product" />
+                          <input type="text" className="product-title" id="product-title" placeholder="Enter Title of Product" />
                           <input type="number" id="user-amount" placeholder="Enter Cost of Product" />
-                          <button class="submit" id="check-amount">Check Amount</button>
+                          <button className="submit" id="check-amount">Add</button>
                       </div>
                   </div>
                   {/* <!-- Output --> */}
-                  <div class="output-container flex-space">
+                  <div className="output-container flex-space">
                       <div>
                           <p>Total Budget</p>
                           <span id="amount">0</span>
@@ -73,18 +53,129 @@ function Dashboard() {
                   </div>
               </div>
               {/* <!-- List --> */}
-              <div class="list">
+              <div className="list">
                   <h3>Expense List</h3>
-                  <div class="list-container" id="list"></div>
+                  <div className="list-container" id="list"></div>
               </div>
           </div>
           {/* <!-- Script --> */}
           <script src="script.js"></script>
-      </body>
-
-      </html>
     </div>
   );
+}
+
+window.onload=function(){
+    
+   
+let totalAmount = document.getElementById("total-amount");
+let userAmount = document.getElementById("user-amount");
+const checkAmountButton = document.getElementById("check-amount");
+const totalAmountButton = document.getElementById("total-amount-button");
+const productTitle = document.getElementById("product-title");
+const errorMessage = document.getElementById("budget-error");
+const productTitleError = document.getElementById("product-title-error");
+const productCostError = document.getElementById("product-cost-error");
+const amount = document.getElementById("amount");
+const expenditureValue = document.getElementById("expenditure-value");
+const balanceValue = document.getElementById("balance-amount");
+const list = document.getElementById("list");
+let tempAmount = 0;
+
+
+//Set Budget Part
+if(totalAmountButton){ 
+    totalAmountButton.addEventListener("click", () => {
+        console.log("here")
+        tempAmount = totalAmount.value;
+    //empty or negative input
+        if (tempAmount === "" || tempAmount < 0) {
+            errorMessage.classList.remove("hide");
+        } else {
+            errorMessage.classList.add("hide");
+        //Set Budget
+            amount.innerHTML = tempAmount;
+        //Set Balance
+            balanceValue.innerText = tempAmount - expenditureValue.innerText;
+        //Clear Input Box
+            totalAmount.value = "";
+        }
+    });
+}
+
+//Function To Disable Edit and Delete Button
+const disableButtons = (bool) => {
+    let editButtons = document.getElementsByClassName("edit");
+    Array.from(editButtons).forEach((element) => {
+        element.disabled = bool;
+    });
+};
+
+//Function To Modify List Elements
+const modifyElement = (element, edit = false) => {
+    let parentDiv = element.parentElement;
+    let currentBalance = balanceValue.innerText;
+    let currentExpense = expenditureValue.innerText;
+    let parentAmount = parentDiv.querySelector(".amount").innerText;
+    if (edit) {
+        let parentText = parentDiv.querySelector(".product").innerText;
+        productTitle.value = parentText;
+        userAmount.value = parentAmount;
+        disableButtons(true);
+    }
+    balanceValue.innerText = parseInt(currentBalance) + parseInt(parentAmount);
+    expenditureValue.innerText =
+        parseInt(currentExpense) - parseInt(parentAmount);
+    parentDiv.remove();
+};
+
+//Function To Create List
+const listCreator = (expenseName, expenseValue) => {
+    let sublistContent = document.createElement("div");
+    sublistContent.classList.add("sublist-content", "flex-space");
+    list.appendChild(sublistContent);
+    sublistContent.innerHTML = `<p class="product">${expenseName}</p><p class="amount">${expenseValue}</p>`;
+    let editButton = document.createElement("button");
+    editButton.classList.add("fa-solid", "fa-pen-to-square", "edit");
+    editButton.style.fontSize = "1.2em";
+    editButton.addEventListener("click", () => {
+        modifyElement(editButton, true);
+    });
+    let deleteButton = document.createElement("button");
+    deleteButton.classList.add("fa-solid", "fa-trash-can", "delete");
+    deleteButton.style.fontSize = "1.2em";
+    deleteButton.addEventListener("click", () => {
+        modifyElement(deleteButton);
+    });
+    sublistContent.appendChild(editButton);
+    sublistContent.appendChild(deleteButton);
+    document.getElementById("list").appendChild(sublistContent);
+};
+
+if(checkAmountButton){
+//Function To Add Expenses
+    checkAmountButton.addEventListener("click", () => {
+    //empty checks
+        if (!userAmount.value || !productTitle.value) {
+            productTitleError.classList.remove("hide");
+            return false;
+        }
+    //Enable buttons
+        disableButtons(false);
+    //Expense
+        let expenditure = parseInt(userAmount.value);
+    //Total expense (existing + new)
+        let sum = parseInt(expenditureValue.innerText) + expenditure;
+        expenditureValue.innerText = sum;
+    //Total balance(budget - total expense)
+        const totalBalance = tempAmount - sum;
+        balanceValue.innerText = totalBalance;
+    //Create list
+        listCreator(productTitle.value, userAmount.value);
+    //Empty inputs
+        productTitle.value = "";
+        userAmount.value = "";
+    });
+}
 }
 
 export default Dashboard;
